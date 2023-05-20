f34642c646895eb294ea4a15cd7adca6900a7082
fi_van <fivan999@yandex.ru>
2023-05-20 21:40:17 +0300

    фиксы

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -101,7 +101,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static_dev',
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
