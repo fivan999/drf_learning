b454b1fff76f0694362b0d3e7756d5e761994a59
fi_van <fivan999@yandex.ru>
2023-05-20 21:18:50 +0300

    inital commit

---
+++ drf/drf/asgi.py
@@ -0,0 +1,8 @@
+import os
+
+import django.core.asgi
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf.settings')
+
+application = django.core.asgi.get_asgi_application()
---
+++ drf/drf/urls.py
@@ -0,0 +1,7 @@
+import django.contrib.admin
+import django.urls
+
+
+urlpatterns = [
+    django.urls.path('admin/', django.contrib.admin.site.urls),
+]
